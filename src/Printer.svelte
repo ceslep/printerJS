33e490e5a6bc53ad34f592a57891f2bd94941e4a
ceslep <ceslep@gmail.com>
2023-03-15 20:04:41 -0500

    Realizado Impresión

---
+++ src/Printer.svelte
@@ -0,0 +1,54 @@
+<script>
+  import printJS from "print-js";
+  //--kiosk-printing
+  let datos = [
+    {
+      item: "Arroz Blanco",
+      precio: 2500,
+    },
+    { item: "Aceite", precio: 12500 },
+    { item: "Sardina", precio: 7500 },
+  ];
+
+  let imprimiendo = false;
+
+  const imprimir = () => {
+    /*  printJS({
+      printable: datos,
+      type: "json",
+      properties: ["item", "precio"],
+      header: "Prueba de Impresion",
+      headerStyle: "font-weight: 300;color:blue;",
+      documentTitle: "",
+      maxWidth:300
+    }); */
+    printJS({
+      printable: "table",
+      type: "html",
+     /*  header: "Prueba de Impresion",
+      headerStyle: "font-weight: 300;color:blue;", */
+      documentTitle: "",
+      maxWidth:300,
+      css:"./printer.css"
+    });
+  };
+</script>
+
+<button class="btn btn-primary" on:click={imprimir}> Imprimir </button>
+
+<table class="table table-bordered table-sm" id="table">
+  <thead>
+    <th>N°</th>
+    <th>Item</th>
+    <th>Valor</th>
+  </thead>
+  <tbody>
+    {#each datos as { item, precio }, index}
+      <tr>
+        <td>{index + 1}</td>
+        <td>{item}</td>
+        <td class="text-end">{precio}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
